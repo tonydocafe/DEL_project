83352c65a6d7fb7a34689f6ae52bee2e773d14f1
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-03-24 19:34:56 -0300

    Create 2D.py

---
+++ drone_environment/2D.py
@@ -0,0 +1,9 @@
+# Instalar o Gymnasium
+#!pip install gymnasium pygame matplotlib
+
+# Importar bibliotecas necessárias
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display
